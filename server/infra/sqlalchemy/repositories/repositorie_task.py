ee85e65cc40abd24314a10344244d7403605f0e0
jailsonzarur <jailson.zarur@ufpi.edu.br>
2024-04-13 17:22:41 -0300

    Adicionado regra de negócio na rota de criar uma nova task.

--- server/infra/sqlalchemy/repositories/repositorie_task.py
+++ server/infra/sqlalchemy/repositories/repositorie_task.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import select, values
+from sqlalchemy import select, values, exists, insert, update
 from server.infra.sqlalchemy.models import model_task
 from server.schemas import schemas_task
 
@@ -11,11 +11,16 @@ class RepositorioTask:
     def listar(self):
         tasks = self.db.query(model_task.Task).all()
         return tasks
-    
+
     def criar(self, task: schemas_task.Task):
+        stmt = select(model_task.Task).filter_by(name = task.name)
+        possiveltask = self.db.execute(stmt).scalars().all()
+        if possiveltask:
+            return {"Message": "This task already exists."}
         db_task = model_task.Task(name = task.name, isDone = task.isDone) 
         self.db.add(db_task)
         self.db.commit()
         self.db.refresh(db_task)
         return db_task
 
+        
